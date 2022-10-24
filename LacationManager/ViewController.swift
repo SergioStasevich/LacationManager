2a4915269222576c11ab25bf5771238735f27853
SergioStasevich <kangurek1991@gmail.com>
2022-10-24 21:58:22 +0200

    audio + video

--- LacationManager/ViewController.swift
+++ LacationManager/ViewController.swift
@@ -1,19 +1,83 @@
-//
-//  ViewController.swift
-//  LacationManager
-//
-//  Created by Siarhei Stasevich on 24/10/2022.
-//
 
 import UIKit
+import CoreLocation
+import AVFoundation
+import AVKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager = CLLocationManager()
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
     }
 
 
+    @IBAction func lacationButton(_ sender: UIButton) {
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation] ){
+        let location: CLLocationCoordinate2D = manager.location!.coordinate
+        print("location - \(location.latitude)\(location.longitude)")
+    }
+    
+//    func playSound() {
+//            guard let url = Bundle.main.url(forResource: "pepas", withExtension: "mp3") else { return }
+//
+//            do {
+//                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+//                try AVAudioSession.sharedInstance().setActive(true)
+//
+//                player = try AVAudioPlayer(contentsOf: url)
+//                if let player = player {
+//                    player.play()
+//                }
+//            } catch let error {
+//                print(error.localizedDescription)
+//            }
+//        }
+    
+    
+    func videoTwo(){
+        guard let url = Bundle.main.url(forResource: "servis", withExtension: "MOV") else { return }
+//        let videoURL = URL(string: "http://techslides.com/demos/sample-videos/small.mp4")
+        let player = AVPlayer(url: url)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(playerLayer)
+        player.play()
+    }
+    
+    func video (){
+        let videoURL = URL(string: "https://www.youtube.com/watch?v=tntNDXg076U&t=16s")
+        let player = AVPlayer(url: videoURL!)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        self.present(playerViewController, animated: true) {
+            playerViewController.player!.play()
+    }
+    }
+    
+    
+    @IBAction func musikButton(_ sender: UIButton) {
+        
+//        playSound()
+    }
+    
+    @IBAction func videoButton(_ sender: UIButton) {
+            video()
+      
+    }
+    
+    
 }
 
